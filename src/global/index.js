f79573b4f1c4ba64fd071e401d878922f1c341ab
Amber Flina <erzz197+git@gmail.com>
2025-07-18 21:22:33 -0300

    Random version changes to fix build

--- src/global/index.js
+++ src/global/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
